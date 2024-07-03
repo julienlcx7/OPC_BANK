fa2d4369e5f543e917277ccbebf8a5bf2cb84fed
julien Lacroix <102021078+julienlcx7@users.noreply.github.com>
2024-07-03 22:41:43 +0200

    Update token.reducer.js

--- frontend/src/reducers/token.reducer.js
+++ frontend/src/reducers/token.reducer.js
@@ -1,13 +1,16 @@
 export const SET_TOKEN = "SET_TOKEN"; // Ensure this is exported if used elsewhere
-
+export const SET_ERROR = "SET_ERROR"; 
 const initialState = {
-    token: null
+    token: null,
+    error: null,
 };
 
 export default function tokenReducer(state = initialState, action){
     switch (action.type) {
         case SET_TOKEN:
-            return { ...state, token: action.payload };
+            return { ...state, token: action.payload,error: null };
+        case SET_ERROR:
+            return {...state,error: action.payload,};
         default:
             return state;
     }
